5310a848d14ed542113621b3b56dbdc3bf91a5b1
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-11-27 18:58:01 -0500

    Added variables for strings

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 from game import HangmanGame
 from ai_player import AIPlayer
+from graph import plot
 from train_model import Train_Model
 import time
 
 def main():
 
+    # Hardcoded word so they both run on the same word. Can change word 
     hangman_word = "army"
-    players = [{"modelPath": "slower_hangman_model.pth", "numWorkers": 2},
-                {"modelPath": "faster_hangman_model.pth", "numWorkers": 8}]
+
+    model_key = "modelPath"
+    num_worker_key = "numWorkers"
+
+    players = [{model_key: "slower_hangman_model.pth", num_worker_key: 2},
+                {model_key: "faster_hangman_model.pth", num_worker_key: 8}]
     
     for player in players:
         start_time = time.time()
 
-        Train_Model(player["modelPath"], player["numWorkers"])
-        player = AIPlayer(player["modelPath"])
+        Train_Model(player[model_key], player[num_worker_key])
+        player = AIPlayer(player[model_key])
 
         game = HangmanGame(player, hangman_word)
         game.play()
@@ -21,6 +27,8 @@ def main():
         end_time = time.time()
         elapsed_time = end_time - start_time
         print(f"Time Played: {elapsed_time} seconds")
+
+    plot(players[0][model_key], players[1][model_key])
 
 if __name__ == '__main__':
     main()
