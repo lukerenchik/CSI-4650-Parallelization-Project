720e4b5f8f1f3a7cc9f3c07e0d81f7fdef753308
Kdot729 <110777824+Kdot729@users.noreply.github.com>
2024-11-27 11:41:16 -0500

    Updated main to use arguments

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 # main.py
+import argparse
 from game import HangmanGame
 from ai_player import AIPlayer
 
-def main():
-    print("Welcome to Hangman!")
-    player = AIPlayer()
+def main(model_filename, num_workers):
+
+    player = AIPlayer(model_filename, num_workers)
 
     game = HangmanGame(player)
     game.play()
 
 if __name__ == '__main__':
-    main()
+
+    # Passing arguments through CLI
+    parser = argparse.ArgumentParser(description="Welcome to Hangman!")
+    parser.add_argument('model_filename', type=str, help="Path to training data")
+    parser.add_argument('num_workers', type=int, help="The numbers of workers")
+    
+    # Get argumetns
+    args = parser.parse_args()
+
+    main(args.model_filename, args.num_workers)
